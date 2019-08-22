8d4acefc63e727f30096e999be00d44f879f7670
Andrew Der <andrew@Andrews-MacBook-Pro-3.local>
2019-08-21 17:53:13 -0700

    added React... WooHoo!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const title = 'React with Webpack and Babel';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+
 var calculateMonthlyPayment = (principal, years, rate) => {
     let monthlyRate = 0
     if (rate) {
