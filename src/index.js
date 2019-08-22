1cce33ca780afcc9f961138c0bb7068dc5e85ff8
Andrew Der <andrew@Andrews-MacBook-Pro-3.local>
2019-08-21 17:43:42 -0700

    adds webpack and babel together, for bundledES6 js code to work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 var calculateMonthlyPayment = (principal, years, rate) => {
+    let monthlyRate = 0
     if (rate) {
-        let monthlyRate = rate / 100 / 12;
+        monthlyRate = rate / 100 / 12;
     }
     var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+    return {principal, years, rate, monthlyPayment, monthlyRate};
 };
 
 document.getElementById('calcBtn').addEventListener('click', () => {
