8c011c1b0dd33d9fe675e2cf64a240a6551cbf3d
Andrew Der <andrew@Andrews-MacBook-Pro-3.local>
2019-08-21 18:00:44 -0700

    basic working react component, on to many more!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import App from './App'
+
 const title = 'React with Webpack and Babel';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  App(title),
   document.getElementById('app')
 );
-
+module.hot.accept();
 
 var calculateMonthlyPayment = (principal, years, rate) => {
     let monthlyRate = 0
